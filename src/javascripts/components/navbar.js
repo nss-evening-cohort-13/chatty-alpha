be8b9b5d5b3cd9cc78d31859043e638978a6ea28
Summer Duke <esrduke@gmail.com>
2020-09-10 18:03:49 -0500

    navbar updates

--- src/javascripts/components/navbar.js
+++ src/javascripts/components/navbar.js
@@ -1,5 +1,5 @@
 const renderNavbar = () => {
-  const domString = $("#navbarContainer").html(`
+  const domString = $('#navbarContainer').html(`
     <div class="fixed-top">
     <div class="collapse" id="navbarToggleExternalContent">
       <div class="bg-dark p-4">
@@ -8,7 +8,8 @@ const renderNavbar = () => {
       </div>
     </div>
     <nav class="navbar navbar-dark bg-dark">
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" 
+      aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
     </nav>
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,1 +1,9 @@
 import '../styles/main.scss';
+
+import navbar from './components/navbar';
+
+const init = () => {
+  navbar.renderNavbar();
+};
+
+init();
