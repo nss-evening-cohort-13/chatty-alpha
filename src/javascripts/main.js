953f226bcd03836c3f12038dddff12629ef0c8d6
Summer Duke <esrduke@gmail.com>
2020-09-10 19:10:57 -0500

    fixed merge conflicts with message display

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,9 +1,12 @@
 import '../styles/main.scss';
 
 import navbar from './components/navbar';
+import _displayMessages from './components/_displayMessages';
+import seedArray from './data/_seedMessage';
 
 const init = () => {
   navbar.renderNavbar();
+  _displayMessages.displayMessages(seedArray.seedData());
 };
 
 init();
