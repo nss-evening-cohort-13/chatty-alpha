ca99e6655ec2465e5ff58e0f716f81fd32e6f97f
Wanda <wmmcinturff@gmail.com>
2020-09-12 12:04:37 -0500

    added logic to disable button

--- src/javascripts/components/_clearAllMessages.js
+++ src/javascripts/components/_clearAllMessages.js
@@ -1,6 +1,12 @@
+import _seedMessage from '../data/_seedMessage';
+
 const clearMessages = () => {
-  $('#clearMssg').on('click', () => $('#messages').remove());
-  $('#clearedMssg').html('Bender!');
+  if (_seedMessage.seedData().length === 0) {
+    $('#clearBtn').attr('disabled', true);
+  } else {
+    $('#clearBtn').on('click', () => $('#messages').remove());
+    $('#clearedMssg').html('Bender!');
+  }
 };
 
 export default { clearMessages };
--- src/javascripts/components/navbar.js
+++ src/javascripts/components/navbar.js
@@ -9,7 +9,7 @@ const renderNavbar = () => {
             <form class="form-inline my-2 my-lg-0">
                 <input class="form-control mr-sm-2" type="text" placeholder="Enter message..." aria-label="Search">
                 <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Enter Message</button>
-                <button class="btn btn-outline-danger my-2 my-sm-0" type="submit">Clear</button>
+                <button class="btn btn-outline-danger my-2 my-sm-0" type="button" id="clearBtn">Clear</button>
             </form>
         </nav>
     </div>
