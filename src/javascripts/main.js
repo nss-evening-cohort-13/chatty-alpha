98e87fdbade2d4a333dba5866ca2bf0767bfde4d
Summer Duke <esrduke@gmail.com>
2020-09-12 12:33:44 -0500

    delete function for messages

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -3,12 +3,14 @@ import '../styles/main.scss';
 import navbar from './components/navbar';
 import _displayMessages from './components/_displayMessages';
 import seedArray from './data/_seedMessage';
-import _clearAllMessages from './components/_clearAllMessages';
+import _deleteMessage from './components/_deleteMessage';
+// import _clearAllMessages from './components/_clearAllMessages';
 
 const init = () => {
   navbar.renderNavbar();
   _displayMessages.displayMessages(seedArray.seedData());
-  _clearAllMessages.clearMessages();
+  // _clearAllMessages.clearMessages();
+  _deleteMessage.deleteMessage(seedArray.seedData());
 };
 
 init();
