ffe2b7c61db3bce953b91d41bf333d0d5bdbdcfa
Wanda <wmmcinturff@gmail.com>
2020-09-12 09:40:57 -0500

    timestamp data modified

--- src/javascripts/data/_seedMessage.js
+++ src/javascripts/data/_seedMessage.js
@@ -3,30 +3,35 @@ const seedArray = [
     id: 1,
     character: 'Bender69',
     message: 'We’ll soon stage an attack on technology worthy of being chronicled in an anthem by Rush!',
+    timestamp: 'September 15, 2019 11:47 PM',
   },
 
   {
     id: 2,
     character: 'TurangaLeela23',
     message: 'I usually try to keep my sadness pent up inside where it can fester quietly as a mental illness.',
+    timestamp: 'November 9, 2019 9:55 PM',
   },
 
   {
     id: 3,
     character: 'Fry-PJ7',
     message: 'Wait, I\'m having one of those things, you know, a headache with pictures.',
+    timestamp: 'May 23, 2020 3:33 AM',
   },
 
   {
     id: 4,
     character: 'DocXoid',
     message: 'I\'m looking for something beautiful and cheap for a lady who is one of those things!',
+    timestamp: 'June 27, 2020 5:44 PM',
   },
 
   {
     id: 5,
     character: 'ProfFarn$worth9',
     message: 'Good news, anyone! The Swedish robot from pi-kea is here with the supercollider I ordered.',
+    timestamp: 'September 3, 2020 8:32 PM',
   },
 
 ];
