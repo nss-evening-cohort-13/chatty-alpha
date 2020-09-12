1428459c3b79592bacc8220dbf3ea3c503dfdea7
Liz <liz@Liz-s-Mac.local>
2020-09-11 19:15:52 -0500

    create enter press function

--- src/javascripts/components/_addNewMessage.js
+++ src/javascripts/components/_addNewMessage.js
@@ -1,25 +1,38 @@
 import seedArray from '../data/_seedMessage';
-import _displayMessages from './_displayMessages';
+import displayMessages from './_displayMessages';
 
-// const clearOutInput = () => {
-
-// };
+const clearOutInput = () => {
+  $('#inputNewMessage').val('');
+};
 
 const addNewMessage = () => {
   const newMessage = {
     id: seedArray.seedData().length + 1,
     character: 'ProfFarn$worth9',
     message: $('#inputNewMessage').val(),
-    // timestamp: moment().format('MMMM Do YYYY, h:mm:ss a'),
+    // timestamp: currentTime.toString,
   };
-  // clearOutInput();
   seedArray.seedData().push(newMessage);
-  _displayMessages.displayMessages(seedArray.seedData());
-  console.log('data', seedArray.seedData());
+  $('#messages').html('');
+  displayMessages.displayMessages(seedArray.seedData());
+  clearOutInput();
+  console.warn('data', seedArray.seedData());
 };
 
 const keyEvent = () => {
-  $('#btnSubmit').click(addNewMessage);
+  // $('#btnSubmit').click(addNewMessage);
+  $('#inputNewMessage').on('keypress', (e) => {
+    if (e.keyCode === 13) {
+      e.preventDefault();
+      console.log('enter clicked');
+      addNewMessage();
+    }
+  });
 };
+
+const m = moment();
+console.warn(m.toString());
+
+keyEvent();
 
 export default { addNewMessage, keyEvent };
