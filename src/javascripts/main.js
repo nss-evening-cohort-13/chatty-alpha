07e4ec2c0deb64f3ae55b2528bc1da07caa8ef73
Wendell <wendell.pattonjr@gmail.com>
2020-09-13 19:24:02 -0500

    trying to get message limit function to work

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -6,9 +6,11 @@ import displayMessages from './components/_displayMessages';
 import addMessage from './components/_addNewMessage';
 import deleteMessage from './components/_deleteMessage';
 import clearAllMessages from './components/_clearAllMessages';
+import _messageLimit from './components/_messageLimit';
 
 const init = () => {
   navbar.renderNavbar();
+  _messageLimit.limitMessages(seedArray.seedData(), 7);
   displayMessages.displayMessages(seedArray.seedData());
   addMessage.keyEvent();
   clearAllMessages.clearMessages();
