66da4ee97a3b8ebb7329d4ba53d2a230c9214ca2
Liz <liz@Liz-s-Mac.local>
2020-09-14 22:09:52 -0500

    fix delete button bug by targeting unique ids

--- src/javascripts/components/_displayMessages.js
+++ src/javascripts/components/_displayMessages.js
@@ -1,14 +1,24 @@
 const displayMessages = (array) => {
-  array.forEach((item, id) => {
+  array.forEach((item) => {
     $('#messages').append(
-      `<div id="messageContainer">
+      `<div id="message-${item.id}">
         <ul id="characterName">${item.character}</ul>
         <ul id="displayMessages">${item.message}</ul>
         <ul id="timestamp">${item.timestamp}</ul>
-        <button id="deleteMessage-${id}" type="button" class="deleteMessage btn btn-primary btn-lg">Delete Message</button>
-    </div>`
+        <button id="deleteMessage-${item.id}" type="button" class="deleteMessage btn btn-primary btn-lg">Delete Message</button>
+      </div>`
     );
   });
 };
+
+// const displayNewMessage = (array) => {
+//   $('#messages').append(
+//     `<div id="message-${item.id}">
+//       <ul id="characterName">${item.character}</ul>
+//       <ul id="displayMessages">${item.message}</ul>
+//       <ul id="timestamp">${item.timestamp}</ul>
+//       <button id="deleteMessage-${item.id}" type="button" class="deleteMessage btn btn-primary btn-lg">Delete Message</button>
+//     </div>`
+// )};
 
 export default { displayMessages };
